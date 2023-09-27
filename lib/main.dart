c5b32102f15f7d67bacda618d07a28cfae83d6ab
cattynip <hi@cattynip.dev>
2023-09-26 21:45:33 -0400

    #2.3 Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:webtoonapp/widgets/button.dart';
-
-class Player {
-  Player({
-    this.name,
-  });
-
-  final String? name;
-}
 
 void main() {
-  var ethan = Player(name: "asjkl");
-  ethan.name;
-
   runApp(const App());
 }
 
@@ -23,78 +11,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 70,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Text(
-                        "Hey, Seol SO",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                      Text(
-                        "Welcome back",
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
-                          fontSize: 14.5,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 120,
-              ),
-              Text(
-                "Total Balance",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white.withOpacity(0.8),
-                ),
-              ),
-              const Text(
-                "\$5 194 482",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.w900,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Button(
-                    text: "Transfer",
-                    bgColor: Color(0xFFF1B33B),
-                    textColor: Colors.black,
-                  ),
-                  Button(
-                    text: "Request",
-                    bgColor: Color(0xFF1F2123),
-                    textColor: Colors.white,
-                  )
-                ],
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text("Hello Flutter!"),
+        ),
+        body: const Center(
+          child: Text("Hello World"),
         ),
       ),
     );
