db20b165c4cbdf01793e1ea49dbac8ac8a2f0a0c
cattynip <hi@cattynip.dev>
2023-10-01 13:05:44 -0400

    #6.5 waitForWebToons

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webtoonapp/screens/home.dart';
-import 'package:webtoonapp/services/api.dart';
 
 void main() {
-  ApiService().getTodaysToons();
   runApp(const App());
 }
 
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:webtoonapp/models/webtoon.dart';
+import 'package:webtoonapp/services/api.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
